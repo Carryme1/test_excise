5c2db316c5be588b134c726b1316f20e0e6191c5
nongnong <862569145@qq.com>
2020-07-14 20:36:38 +0800

    leetcode测试

--- src/main/java/LeetCodetest.java
+++ src/main/java/LeetCodetest.java
@@ -78,22 +78,121 @@ public class LeetCodetest {
         return a;
     }
 
+    public int[] createTargetArray(int[] nums, int[] index) {
+//        一起遍历, 小于length-1插入处理。等于最后一位替换。
+        int[] target = new int[nums.length];
+        int count=0;
+        for (int i=0; i < nums.length; i++){
+//            原target[i]向后一位后，再插入。
+            count=nums.length-index[i];
+            while (count>0) {
+                target[index[i] + 1] = target[index[i]];
+                count--;
+            }
+            target[index[i]]=nums[i];
+        }
+        return target;
+    }
+    public int findNumbers(int[] nums) {
+        //找数组中偶数的个数
+        int re=0;
+        int count=1;
+        int b=0;
+        int a=0;
+        for(int i=0; i<nums.length;i++){
+            b=nums[i];
+            if(b/10==0)
+                continue;
+            while(b/10!=0){
+                b=b/10;
+                count++;
+            }
+            if(count%2==0){
+                re++;
+            }
+            count=1;
+        }
+        return re;
+    }
 
+    public void deleteNode(ListNode node) {
+//        删除链表中的节点
+        while(node.next.next!=null){
+            if(node.next==node)
+                node.next=node.next.next;
+        }
+    }
+    public int minTimeToVisitAllPoints(int[][] points) {
+        int x=0;
+        int y=0;
+        int distances=0;
+//        横纵坐标相减之后，取最大值.按列进行遍历
+        for(int i=1; i<points.length;i++){
+            x=Math.abs(points[i][0]-points[i-1][0]);
+            y=Math.abs(points[i][1]-points[i-1][1]);
+            if(x>=y)
+                distances+=x;
+            else
+                distances+=y;
+        }
+        return distances;
+    }
+
+    public int getDecimalValue(ListNode head) {
+        List<Integer> list=new ArrayList<Integer>();
+        int sum=0;
+        while(head.next!=null){
+            list.add(head.val);
+            head=head.next;
+        }
+        for(int i=0; i<list.size(); i++){
+            sum+=list.get(i)*Math.pow(2,(list.size()-i-1));
+        }
+//        System.out.println(sum);
+        return sum;
+    }
+    public int minimumTotal(List<List<Integer>> triangle) {
+        // 遍历找最小，如果最小与上层的index大于2跳过，找次小。
+        int sum = 0;
+        int min = 0;
+        int init=triangle.get(0).get(0);
+        List<Integer> list;
+        for (int i=1; i<triangle.size();i++){
+            list=triangle.get(i);
+            for(int j=0;j<list.size();j++){
+                if(list.get(i)<=min&&Math.abs(i-j-1)<2)
+                    min=list.get(i);
+            }
+            sum =init+min;
+        }
+        return sum;
+    }
 
 
 
     public static void main(String[] args){
-        int[] nums={1,2,3,4};
+        int[] nums={12,345,2,6,7896};
+        int[] index={0,1,2,2,1};
         String s="lrloseumgh";
 //        int n=234;
         int num=14;
+        int[][] points=new int[][]{{1,1},{3,4},{-1,0}};
         LeetCodetest leetCodetest=new LeetCodetest();
 //        leetCodetest.shuffle(nums,n);
 //        leetCodetest.reverseLeftWords(s,n);
 //        leetCodetest.subtractProductAndSum(n);
 //        leetCodetest.numberOfSteps(num);
-        leetCodetest.decompressRLElist(nums);
+//        leetCodetest.decompressRLElist(nums);
+//        leetCodetest.createTargetArray(nums,index);
+//        leetCodetest.findNumbers(nums);
+//        leetCodetest.minTimeToVisitAllPoints(points);
+//        leetCodetest.getDecimalValue();
+        List<List<Integer>> lists = new ArrayList<List<Integer>>();
+
+        leetCodetest.minimumTotal(lists);
     }
+
+
 
 
 }
